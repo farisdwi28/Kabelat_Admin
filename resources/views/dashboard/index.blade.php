d4e789f6adebebaadb190274c1d403acbdc41078
Faris Dwi Ramadhan <farisdwiramadhan028@gmail.com>
2024-12-20 02:40:18 +0700

    fixing

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.vertical', ['title' => 'Rizz'])
+@extends('layouts.vertical', ['title' => 'Dashboard'])
 
 @section('css')
     @vite(['node_modules/jsvectormap/dist/css/jsvectormap.min.css'])
